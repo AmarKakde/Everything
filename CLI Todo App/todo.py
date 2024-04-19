77ead41abdaf54d8dc098345224afe5c42c70d0a
AmarKakde <amarkakde21@gmail.com>
2024-04-19 20:07:52 +0530

    changes in add_todo() and get_config()

--- CLI Todo App/todo.py
+++ CLI Todo App/todo.py
@@ -6,8 +6,13 @@ import json
 def get_config():
     global FILENAME
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    FILENAME = config['DEFAULT']['todofile']
+    try:
+        config.read('config.ini')
+        FILENAME = config['DEFAULT']['todofile']
+    except FileNotFoundError:
+        pass
+    except AttributeError:
+        pass
     
 def get_args_parser():
     parser = argparse.ArgumentParser(
@@ -15,25 +20,37 @@ def get_args_parser():
                         description='Save, update, delete, display todos')  
     parser.add_argument('-c', '--create', nargs='*', metavar='<todo>', help='enter a todo')
     parser.add_argument('-d', '--delete', nargs=1, type=int, metavar='<int> id of todo', help='please provide the todo id')
-    parser.add_argument('-u', '--update', nargs=2, metavar='<id><status>', help='please provide the id and status for update')
+    parser.add_argument('-u', '--update', nargs=1,type=int, metavar='<id>', help='please provide the id for update')
     parser.add_argument('-ut', '--updatetodo', nargs=2, metavar='<id><todo>', help='please provide the id and todo for update')
     parser.add_argument('-l', '--list', action='store_true', help='display all todos')
     parser.add_argument('-lc', '--listcompleted', action='store_true', help='display all todos with status True')
     parser.add_argument('-lp', '--listpending', action='store_true', help='display all todos with status False')
     return parser
 
+def return_todos():
+    try:
+        with open(FILENAME, 'r') as fp:
+            todo = json.load(fp)
+    except FileNotFoundError as fnfe:
+        print(repr(fnfe))
+    else:
+        return todo
+        
+def write_todos(todo):
+    with open(FILENAME, 'w') as fp:
+        json.dump(todo, fp, indent=4)
+
 def add_todo(todolist):
     todo = None
     
     try:
         if os.path.exists(FILENAME):
-            with open(FILENAME, 'r') as fp:
-                todo = json.load(fp)
+            todo = return_todos()
                 
-                if todo:
-                    last_todo_id = todo[-1]['id']
-                else:
-                    last_todo_id = 1
+            if todo:
+                last_todo_id = todo[-1]['id']
+            else:
+                last_todo_id = 1
             for index in range(len(todolist)):
                 task = {'id': last_todo_id+index+1, 'todo':todolist[index], 'status':False}
                 todo.append(task)
